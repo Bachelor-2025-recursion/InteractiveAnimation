5a58f7229504cfd9c10dd35399a2d05b0fe51a4b
SkarpKat <oash@itu.dk>
2025-03-24 12:46:41 +0100

    Fixed path problems with lib

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(100, 100);
+
+  background(200);
+
+  circle(50, 50, 25);
+
+  describe('A white circle with black outline in the middle of a gray canvas.');
+}
+
+function draw() {
+  // put drawing code here
+}
